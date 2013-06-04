ef46fd3f29381961bfdcf011ef9f183d48a4606f
Brett Hoyer <jbh5079@gmail.com>
2013-06-04 12:22:38 -0700

    fixing yelp search action to include favorite instance

--- app/controllers/yelp_controller.rb
+++ app/controllers/yelp_controller.rb
@@ -14,7 +14,7 @@ class YelpController < ApplicationController
 		@category = params[:category]
 
 		@restaurant_list = Restaurant.where(city_id: City.find_by_name(@location).id , category_id: Category.find_by_name(@category).id)
-
+		@favorite = Favorite.new
 			respond_to do |format|
 				format.js
 				format.html
