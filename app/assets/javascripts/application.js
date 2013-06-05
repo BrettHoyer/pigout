60bffc066c625af1ecc82cd35f16c27e41e0c67f
Brett Hoyer <jbh5079@gmail.com>
2013-06-04 22:18:43 -0700

    adding loading spinner on ajax calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,30 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
+
+
+
+
+$(document).ajaxStart(function(){
+	      var opts = {
+			  lines: 13, // The number of lines to draw
+			  length: 20, // The length of each line
+			  width: 10, // The line thickness
+			  radius: 30, // The radius of the inner circle
+			  corners: 1, // Corner roundness (0..1)
+			  rotate: 0, // The rotation offset
+			  direction: 1, // 1: clockwise, -1: counterclockwise
+			  color: 'white', // #rgb or #rrggbb
+			  speed: 1, // Rounds per second
+			  trail: 60, // Afterglow percentage
+			  shadow: true, // Whether to render a shadow
+			  hwaccel: false, // Whether to use hardware acceleration
+			  className: 'spinner', // The CSS class to assign to the spinner
+			  zIndex: 2e9, // The z-index (defaults to 2000000000)
+			  top: '200', // Top position relative to parent in px
+			  left: '300' // Left position relative to parent in px
+			};
+			var target = document.getElementById('map-canvas');
+			var spinner = new Spinner(opts).spin(target);
+});
+
