65ae16b46e40d9a1ddec773d4dffb66ef8db2e22
Brett Hoyer <jbh5079@gmail.com>
2013-06-17 16:43:36 -0700

    fixing omniauth controller

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,14 @@
+class OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.find_for_facebook_oauth(request.env["omniauth.auth"], current_user)
+
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
+end
